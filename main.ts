abc3e2d692b9d66b9f255cc97e0aee56300f5234
Andrey Tsarev <andrey.tsarev@fpsoftware.io>
2024-11-04 23:46:37 +0000

    Add test capabilities

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
-import {Configuration, MikroORM} from '@dorm/core';
+import {MikroORM} from '@dorm/core';
 import {PostgreSqlDriver} from '@dorm/postgresql';
 import {UserEntity} from "./entity/user.entity.ts";
 import {SessionEntity} from "./entity/session.entity.ts";
 
-const configObj = new Configuration({
+const orm = await MikroORM.init({
     debug: true,
     driver: PostgreSqlDriver,
     password: "postgres",
@@ -14,8 +14,6 @@ const configObj = new Configuration({
     ],
     allowGlobalContext: true,
 });
-
-const orm = await MikroORM.init(configObj);
 
 const user = await orm.em.findOne(UserEntity, { id: 1 }, {
     populate: ["sessions"],
